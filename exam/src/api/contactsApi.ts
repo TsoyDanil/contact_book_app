99ffeb5879ac64f2fa6eedd5a45e688b139e5918
danil tsoy <abdullaevdanil2002@gmail.com>
2023-01-21 12:00:53 +0600

    #1 added all api's actions to get, adelete, update and change contact

--- exam/src/api/contactsApi.ts
+++ exam/src/api/contactsApi.ts
@@ -1,6 +1,8 @@
 import IContactResponse from "../interfaces/IContactResponse";
 import {AxiosResponse} from 'axios'
 import { contactsInstance } from "./instances";
+import ICombinedContactData from "../interfaces/ICombinedContactData";
+import IContact from "../interfaces/IContact";
 
 class ContactsApi {
     public getContacts = async(): Promise<IContactResponse | void> => {
@@ -8,6 +10,31 @@ class ContactsApi {
             const response: AxiosResponse<IContactResponse | void> = await contactsInstance.get('.json')
             return response.data 
         } catch(error: unknown){
+            console.log(error)
+        }
+    }
+
+    public updateContact = async (dataObject: ICombinedContactData) => {
+        try{
+            await contactsInstance.put('/' + dataObject.id + '.json', dataObject.contactData)
+        } catch(error: unknown){
+            console.log(error)
+        }
+    }
+
+    public createContact = async (contact: IContact) => {
+        try{
+            await contactsInstance.post('.json', contact)
+        } catch(error: unknown){
+            console.log(error)
+        }
+    }
+
+    public deleteContact = async(id: string | undefined) => {
+        try{
+            await contactsInstance.delete('/' + id + '.json' )
+        }
+        catch(error:unknown){
             console.log(error)
         }
     }
