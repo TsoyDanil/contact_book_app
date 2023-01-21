f86f7a934b8bb0e5bcc686b44b671480da30cba1
danil tsoy <abdullaevdanil2002@gmail.com>
2023-01-21 12:14:23 +0600

    #1 added actions and cases for all api's actions

--- exam/src/store/contacts/contacts.slice.ts
+++ exam/src/store/contacts/contacts.slice.ts
@@ -1,5 +1,7 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit'
 import { contactsApi } from '../../api/contactsApi'
+import ICombinedContactData from '../../interfaces/ICombinedContactData'
+import IContact from '../../interfaces/IContact'
 import IContactResponse from '../../interfaces/IContactResponse'
 import { createAppAsyncThunk } from '../createAppAsyncThunk'
 import IContactsState from './IContactsState'
@@ -10,6 +12,30 @@ export const getContacts = createAppAsyncThunk(
     `${namespace}/getContacts`,
     async (): Promise<IContactResponse | void> => {
         return await contactsApi.getContacts()
+    }
+)
+
+export const updateContact = createAppAsyncThunk(
+    `${namespace}/updateContact`,
+    async(dataObject: ICombinedContactData, {dispatch}): Promise<void> => {
+        await contactsApi.updateContact(dataObject)
+        dispatch(getContacts())
+    }
+)
+
+export const deleteContact = createAppAsyncThunk(
+    `${namespace}/deleteContact`,
+    async (id: string | undefined, {dispatch}): Promise<void> => {
+        await contactsApi.deleteContact(id)
+        dispatch(getContacts())
+    }
+)
+
+export const createContact = createAppAsyncThunk(
+    `${namespace}/createContact`,
+    async (contact: IContact, {dispatch}) => {
+        await contactsApi.createContact(contact)
+        dispatch(getContacts())
     }
 )
 
@@ -35,6 +61,33 @@ export const contactsSlice = createSlice({
             if (action.payload){
                 state.contacts = action.payload
             }
+        })
+        .addCase(updateContact.pending, (state: IContactsState) => {
+            state.loading = true
+        })
+        .addCase(updateContact.rejected, (state: IContactsState) => {
+            state.loading = false
+        })
+        .addCase(updateContact.fulfilled, (state: IContactsState) => {
+            state.loading = false
+        })
+        .addCase(deleteContact.pending, (state: IContactsState) => {
+            state.loading = true
+        })
+        .addCase(deleteContact.rejected, (state: IContactsState) => {
+            state.loading = false
+        })
+        .addCase(deleteContact.fulfilled, (state: IContactsState) => {
+            state.loading = false
+        })
+        .addCase(createContact.pending, (state: IContactsState) => {
+            state.loading = true
+        })
+        .addCase(createContact.rejected, (state: IContactsState) => {
+            state.loading = false
+        })
+        .addCase(createContact.fulfilled, (state: IContactsState) => {
+            state.loading = false
         })
     }
 })
